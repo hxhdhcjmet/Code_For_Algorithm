00492aded1e6ed860469e4a9fac2624f5d611b9d
Hxhdhcjmet <2871148635@qq.com>
2025-04-07 22:15:35 +0800

    暂未完成

---
+++ 单调队列/滑动窗口.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<deque>
+#include<vector>
+using namespace std;
+auto maxWindowData(vector<int>& nums,int k){
+    vector<int> result;
+    deque<int> window;
+    for(int i=0;i<nums.size();i++){
+        while(!window.empty()&&)
+    }
+
+}
